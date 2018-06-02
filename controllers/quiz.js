be81ee07d264a909db49724e090aa895375515da
juanalvarez96 <juanitoafv@gmail.com>
2018-06-02 16:30:40 +0200

    Funcionamiento no completamente correcto

--- controllers/quiz.js
+++ controllers/quiz.js
@@ -153,3 +153,129 @@ exports.check = (req, res, next) => {
         answer
     });
 };
+
+//Randomplay
+exports.randomPlay = (req,res,next) => {
+    let toBeResolved = [];
+    let score;
+    score = req.session.score;
+    toBeResolved = req.session.randomPlay;
+    models.quiz.findAll()
+        .then(quizzes => {
+            // 1 Caso, empiezo la primera partida
+            if(score==undefined || toBeResolved==undefined){
+                toBeResolved=[];
+                score=0;
+                for (i=0;i<quizzes.length;i++){
+                    toBeResolved[i]=quizzes[i].id;
+                }
+                score=0;
+
+                //Lanzamos primera pregunta
+                let indice = Math.floor(Math.random() * toBeResolved.length);
+                let id = toBeResolved[indice];
+                toBeResolved.splice(indice, 1);
+                req.session.randomPlay=toBeResolved;
+                req.session.score=score;
+                validateId(id)
+                    .then(id =>{
+                        models.quiz.findById(id)
+                            .then(quiz => {
+                                res.render('quizzes/random_play', {
+                                    quiz:quiz,
+                                    score:score
+                            })
+
+                        })
+                    })
+
+            }
+            if(score<quizzes.length && toBeResolved !== "undefined"){
+                //Lanzamos primera pregunta
+                let indice = Math.floor(Math.random() * toBeResolved.length);
+                let id = toBeResolved[indice];
+                toBeResolved.splice(indice, 1);
+                req.session.randomPlay=toBeResolved;
+                validateId(id)
+                    .then(id =>{
+                        models.quiz.findById(id)
+                            .then(quiz => {
+                                res.render('quizzes/random_play', {
+                                    quiz:quiz,
+                                    score:score
+                                })
+
+                            })
+                    })
+
+            }
+            else {
+                console.log(toBeResolved);
+                let id = toBeResolved[0];
+                req.session.randomPlay = undefined;
+                validateId(id)
+                    .then(id =>{
+                        models.quiz.findById(id)
+                            .then(quiz => {
+                                res.render('quizzes/random_play', {
+                                    quiz:quiz,
+                                    score:score
+                                })
+
+                            })
+                    })
+            }
+        })
+
+
+};
+
+exports.randomCheck = (req,res,next)=>{
+    const {quiz, query} = req;
+
+    const answer = query.answer || "";
+    const result = answer.toLowerCase().trim() === quiz.answer.toLowerCase().trim();
+
+    if (result){
+        req.session.score++;
+    }
+    models.quiz.findAll()
+        .then(quizzes=> {
+            if(req.session.score<quizzes.length) {
+                res.render('quizzes/random_result', {
+                    result: result,
+                    answer: answer,
+                    score: req.session.score
+                })
+            }
+            else{
+                res.render('quizzes/random_nomore', {
+                    score : req.session.score
+                })
+            }
+
+        })
+
+};
+
+const validateId = id => {
+    return new Promise((resolve, reject) => {
+        models.quiz.findById(id)
+            .then(quiz => {
+                if (typeof id === 'undefined') {
+                    reject(new Error(`Falta el parámetro <id>`));
+                }
+                if (!quiz) {
+                    reject(new Error("Quiz no encontrado"));
+                } else {
+                    id = parseInt(id);
+                    if (Number.isNaN(id)) {
+                        reject(new Error(`EL valor del parámetro <id> no es un número.`));
+                    } else {
+                        resolve(id);
+                    }
+                }
+            })
+
+    })
+};
